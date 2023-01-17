ba8822e3a41b83371f63833d9ac05b787eea48c2
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-17 00:06:54 -0600

    UI update
     + Added Tasks and Notes section.
    
     + Added pause button
    
     +  Fixed header border bottom
    
     + Added border to top buttons.

---
+++ main.js
@@ -0,0 +1,9 @@
+//  Get the timer working on start click for Pomodoro section.
+//  Get the short and long break to swtich times Short(5 min) long (10 min)
+//  Get the short and long timers working.
+
+
+
+// Timer input elements \\
+const startBtn = document.getElementById('start-btn');
+const pauseBtn = document.getElementById('pause-btn');
