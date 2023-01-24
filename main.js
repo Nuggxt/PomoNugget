382a80eff35ff54237f67e87518f356324c5984e
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-24 00:06:49 -0600

    Minor update to timer
    Timer has an update now.
    
    The user can click on one of three buttons to select the timer style they want. and it puts the time into the display.
    
    pomodoro = 25
    short = 5
    long = 15

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //  Get the timer working on start click for Pomodoro section.
-//  Get the short and long break to swtich times Short(5 min) long (10 min)
+//  Get the short and long break to swtich times Short(5 min) long (15 min)
 //  Get the short and long timers working.
 
 
@@ -7,10 +7,21 @@
 // Timer input elements \\
 let startBtn = document.querySelector('#start-btn');
 let pauseBtn = document.querySelector('#pause-btn');
+let pomodoro = '25:00';
+let shortButton = '5:00';
+let longButton = '15:00';
 
-const timer = {
-    pomodoro: 25,
-    shortBreak: 5,
-    longBreak: 15,
+
+// Functions for the 3 different timers.
+function pomoBtn() {
+  document.getElementById('timer').innerHTML = pomodoro;
+};
+
+function shortBtn() {
+  document.getElementById('timer').innerHTML = shortButton;
+};
+
+function longBtn() {
+  document.getElementById('timer').innerHTML = longButton;
 };
 
