2ac302f22e11e132399f4ed0e4debc14163a23ef
NuggxtDev <devnuggetcodes@gmail.com>
2023-03-06 23:14:37 -0600

    got the timer buttons to show time on click.

---
+++ main.js
@@ -0,0 +1,20 @@
+        // Timer Section \\
+
+let pomoTime = 25;
+let shortBtn = 5;
+let longBtn = 15;
+
+function pomo() {
+  document.getElementById('minutes').innerText = 25;
+};
+
+function short() {
+  document.getElementById('minutes').innerText = 5;
+};
+
+function long() {
+  document.getElementById('minutes').innerText = 15;
+};
+
+
+        // Tasks Section \\
